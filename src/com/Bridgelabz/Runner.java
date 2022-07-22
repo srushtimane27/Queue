280b381a066475d1e71de746af3cd1fdc8dc2a85
srushtimane27 <srushtimane31@gmail.com>
2022-07-22 22:02:07 +0530

    Queue Operations

---
+++ src/com/Bridgelabz/Runner.java
@@ -0,0 +1,14 @@
+package com.Bridgelabz;
+
+public class Runner {
+    public static void main(String[] args) {
+
+
+        Queue q = new Queue();
+        q.enQueue(56);
+        q.enQueue(30);
+        q.enQueue(70);
+
+        q.show();
+    }
+}
